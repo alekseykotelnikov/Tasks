67886686c1ab1f55d2af479938f37cb9dcc1d7d6
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-01 13:20:11 +0300

    Выполнил восьмое упражнение

---
+++ Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Все целые числа диапозона
+int count = 0;
+int number = 1;
+while(count < 20)
+{
+    if(number % 2 == 0)
+    {
+        Console.WriteLine(number);
+    }
+    count++;
+    number++;
+}
