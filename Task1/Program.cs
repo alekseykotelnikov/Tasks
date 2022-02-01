06f4aa93cf9eb4d73063c25aef584e46b71c7dea
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-01 11:23:18 +0300

    Второе задание с двумя числами, является ли первое число квадратом втрого

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Является ли первое квадратом второго
+Console.Write("Введите первое число ");
+string numberAStr = Console.ReadLine();
+double numberA = double.Parse(numberAStr);
+Console.Write("Введите второе число ");
+string numberBStr = Console.ReadLine();
+double numberB = double.Parse(numberBStr);
+if(numberA / numberB == numberB)
+
+    Console.WriteLine("True ");
+
+else
+
+    Console.WriteLine("False ");
