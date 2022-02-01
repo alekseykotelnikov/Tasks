81a5f412f5170fd549182456c5b2ca69ad75f9c7
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-01 12:07:26 +0300

    Добавили задание с большим и меньшим числом из двух

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Даны два числа, вычислите большее и меньшее число
+Console.Write("Введите первое число ");
+string numberAStr = Console.ReadLine();
+double numberA = double.Parse(numberAStr);
+Console.Write("Введите второе число ");
+string numberBStr = Console.ReadLine();
+double numberB = double.Parse(numberBStr);
+if(numberB == numberA)
+{
+    Console.WriteLine("Числа одинаковые");
+}
+else
+{
+    if(numberA - numberB < 0)
+    {
+    Console.Write("Большее второе число ");
+    Console.WriteLine(numberB);
+    Console.Write("Меньшее первое число ");
+    Console.WriteLine(numberA);
+    }
+    else
+    {
+    Console.Write("Большее второе число ");
+    Console.WriteLine(numberA);
+    Console.Write("Меньшее первое число ");
+    Console.WriteLine(numberB);
+    }
+}
