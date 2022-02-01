5ed3c04f8f215fc195bce91d46a4d5d6096d1fcb
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-01 10:39:46 +0300

    Сделали упражнение на квадрат и написали комментарии

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿// Kvadrat number;
+Console.Write("Введите число "); // Вводим возможность ввода числа
+string numberStr = Console.ReadLine(); // Задаем название для считываемой строки (все что вводим в консоль это строки, а не числа)
+double number = double.Parse(numberStr); // с помощью функции Parse перевоим строку в число
+Console.WriteLine (number * number); // Выводим результат
