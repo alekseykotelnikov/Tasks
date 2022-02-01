8ef345a02b9dc234de36794b9eca05316e421bef
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-01 14:42:30 +0300

    Сделал все упражнения из первого домашнего задания

---
+++ Task6_Chetnoe_chislo/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+Console.Write("Введите число ");
+string numberAStr = Console.ReadLine();
+double numberA = double.Parse(numberAStr);
+if(numberA % 2 == 0)
+{
+    Console.WriteLine("Данное число является четным");
+}
+else
+{
+    Console.WriteLine("Данное число является нечетным");
+}
