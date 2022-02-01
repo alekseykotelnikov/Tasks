e1062fa9ea53c357b8d9e377753628cedd1cc670
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-01 14:35:45 +0300

    Сделал 4 упражнение большее из 3 чисел

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти максимальное из трех чисел
+Console.Write("Введите первое число ");
+string numberAStr = Console.ReadLine();
+double numberA = double.Parse(numberAStr);
+Console.Write("Введите второе число ");
+string numberBStr = Console.ReadLine();
+double numberB = double.Parse(numberBStr);
+Console.Write("Введите третье число ");
+string numberCStr = Console.ReadLine();
+double numberC = double.Parse(numberCStr);
+double max = numberA;
+if(numberB > numberA) max = numberB;
+if(numberC > numberB) max = numberC;
+Console.Write("Большее число это ");
+Console.WriteLine(max);
