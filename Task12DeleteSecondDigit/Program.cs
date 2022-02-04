92eda2d278d51b61d115170e0c05988ad0010848
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-04 12:12:04 +0300

    Выполнил задание 12, удаление второй цифры из трехзначного числа

---
+++ Task12DeleteSecondDigit/Program.cs
@@ -0,0 +1,6 @@
+﻿// Удалить вторую цифру трёхзначного числа
+int randomNumber = new Random().Next(100, 1000);
+Console.WriteLine($"Трехзначное число, это {randomNumber}");
+int lastDigit = randomNumber % 10;
+int firstDigit = randomNumber / 100;
+Console.WriteLine($"Трехзначное число без второй цифры, это {firstDigit}{lastDigit}");
