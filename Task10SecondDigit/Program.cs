fde38ffda5407dcb5bfdac0b0a96a49ead5dda5e
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-04 11:08:56 +0300

    Выполнил задание 10, вывел второе число из трехзначного

---
+++ Task10SecondDigit/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+int randomNumber = new Random().Next(100, 1000);
+Console.WriteLine($"Трехзначное число {randomNumber}");
+int lastDigit = randomNumber / 10;
+int secondDigit = lastDigit % 10;
+Console.WriteLine($"Второе число в трехзначном это {secondDigit}");
