ddc3203a31342220513ddf2e2360f996732ecb32
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-04 13:31:19 +0300

    Выполнил задание 13, кратно ли число заданному с выводом остатка через void

---
+++ Task13Multiple/Program.cs
@@ -0,0 +1,29 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+void Multiple(int arg1, int arg2)
+{
+    if (arg1 % arg2 == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        int result = arg1 % arg2;
+        Console.WriteLine($"Остаток {result}");
+    }
+}
+
+int RandomNumber()
+{
+    int randomNumber = new Random().Next(10, 100);
+    return randomNumber;
+}
+
+int specifiedNumber = RandomNumber();
+Console.WriteLine($"Заданное число, это {specifiedNumber}");
+int checkNumber = RandomNumber();
+Console.WriteLine($"Проверяемое число, это {checkNumber}");
+
+Multiple(checkNumber,specifiedNumber);
+
+Console.WriteLine($"{Multiple}");
