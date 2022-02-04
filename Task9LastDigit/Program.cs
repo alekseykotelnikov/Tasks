15429a877ef968e866120640096eadc3b0d33a5c
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-04 10:58:41 +0300

    Выполнил задание 9 последнее число в трехзначном

---
+++ Task9LastDigit/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int randomNumber = new Random().Next(100, 1000);
+Console.WriteLine($"Трехзначное число {randomNumber}");
+int lastDigit = randomNumber % 10;
+Console.WriteLine($"Последнее число в трехзначном это {lastDigit}");
