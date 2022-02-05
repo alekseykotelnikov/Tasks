69ef59085d3f0099fd9257e9680a51d9bb4085c5
aleksey <aleksey7008795@yandex.ru>
2022-02-05 12:55:44 +0300

    Решена 14 задача через ввод массива

---
+++ Task14FindThirdDigit/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int[] array = { 25, 159, 1, 0, -258, -100, 345, -876 };
+int n = array.Length;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] < 100 && array[index] > -100)
+    {
+        Console.WriteLine("Третьей цифры в числе нет");
+    }
+    else
+    {
+        int findThirdDigit = array[index] % 10;
+
+        if (findThirdDigit < 0)
+        {
+            findThirdDigit = findThirdDigit * -1;
+        }
+        Console.WriteLine($"Третья цифра в числе {findThirdDigit}");
+    }
+    index++;
+}
