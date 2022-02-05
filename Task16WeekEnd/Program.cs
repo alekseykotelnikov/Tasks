8cb8e94fca4d40f150967d0675182df0c9587eac
aleksey <aleksey7008795@yandex.ru>
2022-02-05 13:49:29 +0300

    Выполнена 16 задача через bool для закрепления, поменял местами функциюи условие

---
+++ Task16WeekEnd/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+/* 1-Monday; 2-Tuesday;
+3-Wednesday; 4-Thursday;
+5-Friday; 6-Saturday;7-Sunday;
+Weekend - Saturday and Sunday;*/
+
+int randomNumber = new Random().Next(1, 8);
+
+bool Weekend(int number)
+{
+    return randomNumber > 5;
+}
+
+if (Weekend(randomNumber))
+{
+    Console.WriteLine($"Номер дня недели {randomNumber} является выходным");
+}
+else
+{
+    Console.WriteLine($"Номер дня недели {randomNumber} является не выходным");
+}
