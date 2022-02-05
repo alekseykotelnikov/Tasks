277682936c6b5a8cde9dc4ecdd54d4c81a44ce88
aleksey <aleksey7008795@yandex.ru>
2022-02-05 13:23:11 +0300

    Сделал 15 задачу через bool

---
+++ Task15CheckIfModed/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+int randomNumber = new Random() .Next();
+Console.WriteLine($"Дано число {randomNumber}");
+
+if (CheckIfModed(randomNumber))
+{
+    Console.WriteLine($"Число {randomNumber} делится на 7 и на 23");
+}
+else
+{
+    Console.WriteLine($"Число {randomNumber} не делится на 7 и на 23");
+}
+
+bool CheckIfModed(int randomNumber)
+{
+    return randomNumber % (7 *23) == 0;
+}
