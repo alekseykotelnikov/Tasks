d5762854d97a2795d078bd69cf3ea27f0e94b3bd
aleksey <aleksey7008795@yandex.ru>
2022-02-08 23:14:46 +0300

    Сделана задача 20 с рандомом

---
+++ Task20QuaterRangeFindquater/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+int quaterNumber = new Random() .Next(1, 5);
+string range = " ";
+if (quaterNumber == 1)
+{
+    range = "x > 0; y > 0";
+}
+if (quaterNumber == 2)
+{
+    range = "x < 0; y > 0";
+}
+if (quaterNumber == 3)
+{
+    range = "x < 0; y < 0";
+}
+if (quaterNumber == 4)
+{
+    range = "x > 0; y < 0";
+}
+
+Console.WriteLine($"Четверть номер {quaterNumber} имеет следующий диапазон для х и у: {range} ");
