dfe73801af14f976eef4460d7bf675701e78b33b
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-08 12:22:05 +0300

    Выполнена задача 5 с применением TryParse

---
+++ Task5F(x)/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+
+int F(int arg)
+{
+    int result = arg * arg;
+    return result;
+}
+    
+Console.Write("Введите число ");
+string numberAStr = Console.ReadLine();
+if(int.TryParse(numberAStr, out int a))
+{
+    int y = F(a);
+    Console.WriteLine($"Значение функции y = f(a) будет {y}");
+}
+else
+{
+    Console.WriteLine($"Введено не число");
+}
