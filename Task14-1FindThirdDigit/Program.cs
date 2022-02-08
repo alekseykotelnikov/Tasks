2d432c0b8e0151b81a7c07841f1530ab7635f4d7
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-08 11:32:14 +0300

    Добавил иное решение для задачи 14 с семинара

---
+++ Task14-1FindThirdDigit/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти третью цифру числа или сообщить, что её нет? Вариант уровня GODMODE 
+
+Console.Write("Введите число ");
+string valueStr = Console.ReadLine();
+if (int.TryParse(valueStr, out int valueInt))
+{
+    if (valueStr.Length >= 3)
+    {
+        Console.WriteLine($"Третья цифра числа {valueStr[2]}");
+    }
+    else
+    {
+        Console.WriteLine("Число не трехзначное");
+    }
+}
+else
+{
+    Console.WriteLine("Ввели не число");
+
+}
