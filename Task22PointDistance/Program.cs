6cac97d326d6e3ff457b8335f2bfa75fcb0ae08e
aleksey <aleksey7008795@yandex.ru>
2022-02-09 01:05:34 +0300

    Сделал невозможное и закончил задачу 22 с точками в 3д

---
+++ Task22PointDistance/Program.cs
@@ -0,0 +1,30 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+double num = 0;
+double Coards(double arg)
+{
+    return new Random().Next(-10, 10);
+}
+
+double Sum (double first, double second)
+{
+    double sum = (second - first) * (second - first);
+    return sum;
+}
+
+
+double x1 = Coards(num);
+double x2 = Coards(num);
+double y1 = Coards(num);
+double y2 = Coards(num);
+double z1 = Coards(num);
+double z2 = Coards(num);
+
+Console.WriteLine($"Координата x1={x1}, x2={x2}, y1={y1}, y2={y2}, z1={z1}, z2={z2}");
+
+
+double distanceX = Sum(x1, x2);
+double distanceY = Sum(y1, y2);
+double distanceZ = Sum(z1, z2);
+
+double finallDistance = Math.Sqrt( distanceX + distanceY + distanceZ);
+Console.WriteLine($"Расстояние между точками = {finallDistance}");
