3f0fafd90bb82149b5d8c5addf6fa1f523a47abb
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-09 09:55:09 +0300

    В первую задачу добавлена защита от овоща

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,11 +1,26 @@
 ﻿// Является ли первое квадратом второго
-Console.Write("Введите первое число ");
-string numberAStr = Console.ReadLine();
-double numberA = double.Parse(numberAStr);
-Console.Write("Введите второе число ");
-string numberBStr = Console.ReadLine();
-double numberB = double.Parse(numberBStr);
-if(numberA / numberB == numberB)
+
+
+int numberA = 0;
+int numberB = 0;
+
+while (numberA == 0)
+{
+    Console.Write("Введите первое число ");
+    string? numberAStr = Console.ReadLine();
+    int.TryParse(numberAStr, out numberA);
+}
+
+while (numberB == 0)
+{
+    Console.Write("Введите второе число ");
+    string? numberBStr = Console.ReadLine();
+    int.TryParse(numberBStr, out numberB);
+}
+
+
+
+if (numberA / numberB == numberB)
 
     Console.WriteLine("True ");
 
