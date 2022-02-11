766d0135b9a22c92f8ac89dc3e6f6515f7974c6b
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-11 11:21:11 +0300

    Отделил функции/методы от кода

--- Task16WeekEnd/Program.cs
+++ Task16WeekEnd/Program.cs
@@ -5,12 +5,12 @@
 5-Friday; 6-Saturday;7-Sunday;
 Weekend - Saturday and Sunday;*/
 
-int randomNumber = new Random().Next(1, 8);
-
 bool Weekend(int number)
 {
-    return randomNumber > 5;
+    return number > 5;
 }
+
+int randomNumber = new Random().Next(1, 8);
 string[] nameDay = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
 if (Weekend(randomNumber))
 {
@@ -20,3 +20,4 @@ else
 {
     Console.WriteLine($"День {nameDay[randomNumber - 1]} под цифрой {randomNumber} не является выходным");
 }
+
