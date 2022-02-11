fdf3528d7e8b66c1227292e46633411aa774e5a7
aleksey <aleksey7008795@yandex.ru>
2022-02-11 21:49:08 +0300

    Выполнена задача 29 через функции

---
+++ Task29MultipleResult/Program.cs
@@ -0,0 +1,25 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+int InPut(string message)
+{
+    Console.WriteLine(message);
+    string numStr = Console.ReadLine();
+    int numInt = int.Parse(numStr);
+    return numInt;
+}
+
+int Multiple (int number)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result = i * result;
+    }
+    return result;
+}
+
+int numberFirst = InPut("Введите предельное число ");
+
+int multipleResult = Multiple(numberFirst);
+
+Console.WriteLine($"Результат произведения = {multipleResult}");
