d7aac2b1e74d63b3ffd5e13e312756b166144c12
aleksey <aleksey7008795@yandex.ru>
2022-02-11 21:35:41 +0300

    Задача 27 выполнена через Рандом

---
+++ Task27CountOfDigit/Program.cs
@@ -0,0 +1,20 @@
+﻿// Определить количество цифр в числе
+
+int GetNumber()
+{
+    int randomNumber = new Random().Next(100, 10000);
+    return randomNumber;
+}
+
+int number = GetNumber();
+Console.WriteLine(number);
+
+int countOfDigit = 0;
+
+while (number != 0)
+{
+    number = number / 10;
+    countOfDigit++;
+}
+
+Console.WriteLine($"Количество цифр в числе будет {countOfDigit}");
