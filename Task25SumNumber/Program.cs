d69758e9a4344875a5a69a3f699afb248f69439e
aleksey <aleksey7008795@yandex.ru>
2022-02-11 21:22:23 +0300

    Решена 25 задача через функцию

---
+++ Task25SumNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти сумму чисел от 1 до А
+
+int InPut(string message)
+{
+    Console.WriteLine(message);
+    string numStr = Console.ReadLine();
+    int numInt = int.Parse(numStr);
+    return numInt;
+}
+
+int number  = InPut("Введите предельное число");
+
+int sum = 0;
+
+for (int i = 1; i <= number; i ++)
+{
+    sum = sum + i;
+}
+
+Console.WriteLine($"Сумма будет = {sum}");
