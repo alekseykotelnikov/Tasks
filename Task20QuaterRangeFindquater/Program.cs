f20ee3c771a472de5faefb4636d397a98ada0d81
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-11 12:31:32 +0300

    Отделил функции от кода

--- Task20QuaterRangeFindquater/Program.cs
+++ Task20QuaterRangeFindquater/Program.cs
@@ -1,32 +1,36 @@
 ﻿// Задать номер четверти, показать диапазоны для возможных координат
 
+void QuaterNumber(int number, string str1, string str2)
+{
+    if (number == 1)
+    {
+        str1 = "I";
+        str2 = "x > 0; y > 0";
+        Console.WriteLine($"Четверть номер {str1} имеет следующий диапазон для х и у: {str2} ");
+    }
+    if (number == 2)
+    {
+        str1 = "II";
+        str2 = "x < 0; y > 0";
+        Console.WriteLine($"Четверть номер {str1} имеет следующий диапазон для х и у: {str2} ");
+    }
+    if (number == 3)
+    {
+        str1 = "III";
+        str2 = "x < 0; y < 0";
+        Console.WriteLine($"Четверть номер {str1} имеет следующий диапазон для х и у: {str2} ");
+    }
+    if (number == 4)
+    {
+        str1 = "IV";
+        str2 = "x > 0; y < 0";
+        Console.WriteLine($"Четверть номер {str1} имеет следующий диапазон для х и у: {str2} ");
+    }
+}
+
+
 int quaterNumberInt = new Random().Next(1, 5);
 Console.WriteLine($"Задана четверть номер {quaterNumberInt}");
 string range = " ";
 string quaterNumberStr = " ";
-
-void QuaterNumber(int number)
-{
-    if (quaterNumberInt == 1)
-    {
-        quaterNumberStr = "I";
-        range = "x > 0; y > 0";
-    }
-    if (quaterNumberInt == 2)
-    {
-        quaterNumberStr = "II";
-        range = "x < 0; y > 0";
-    }
-    if (quaterNumberInt == 3)
-    {
-        quaterNumberStr = "III";
-        range = "x < 0; y < 0";
-    }
-    if (quaterNumberInt == 4)
-    {
-        quaterNumberStr = "IV";
-        range = "x > 0; y < 0";
-    }
-}
-QuaterNumber(quaterNumberInt);
-Console.WriteLine($"Четверть номер {quaterNumberStr} имеет следующий диапазон для х и у: {range} ");
+QuaterNumber(quaterNumberInt, quaterNumberStr, range);
