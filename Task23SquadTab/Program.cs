4a2ad4d72e987926631c464ef9b72350f5e4a85f
aleksey <aleksey7008795@yandex.ru>
2022-02-11 21:14:05 +0300

    Выполнена задача 23 через массивы

---
+++ Task23SquadTab/Program.cs
@@ -0,0 +1,31 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+int InPut(string message)
+{
+    Console.WriteLine(message);
+    string numStr = Console.ReadLine();
+    int numInt = int.Parse(numStr);
+    return numInt;
+}
+
+int[] InitArray(int number)
+{
+    int[] array = new int[number + 1];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = i * i;
+    }
+    return array;
+}
+
+void PrintArray(int[] argarray)
+{
+    for (int i = 1; i < argarray.Length; i++)
+    {
+        Console.WriteLine(argarray[i]);
+    }
+}
+
+int b = InPut("Введите число > 1");
+var array = InitArray(b);
+PrintArray(array);
