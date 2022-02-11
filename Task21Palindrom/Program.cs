bb936301d2817c99f7a88633f969279ce509199c
aleksey <aleksey7008795@yandex.ru>
2022-02-11 20:22:18 +0300

    решена задача 21 через bool

---
+++ Task21Palindrom/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+int InPut(string message)
+{
+    Console.WriteLine(message);
+    string numStr = Console.ReadLine();
+    int numInt = int.Parse(numStr);
+    return numInt;
+}
+
+int palindrom = InPut("Введите пятизначное число ");
+
+bool digitOne = palindrom % 10 == palindrom / 10000;
+
+bool digitTwo = (palindrom / 10) % 10 == (palindrom / 1000) % 10;
+
+if (digitOne && digitTwo)
+{
+    Console.WriteLine("Число является палиндромом ");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом ");
+}
