510b25786bdf1ee5b303e4ce0ce3c579ebb2a524
aleksey <aleksey7008795@yandex.ru>
2022-02-12 16:20:17 +0300

    Выполнена задача 28 через цикл, с защитой от дурака

---
+++ Task28SumDigitInNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿// Подсчитать сумму цифр в числе
+int number = -1;
+while (number < 0)
+{
+    Console.Write("Введите положительное число, чтобы не спорить о знаке \" - \"  ");
+    string? numberAStr = Console.ReadLine();
+    int.TryParse(numberAStr, out number);
+}
+
+Console.Write($"Сумма цифр в числе {number} = ");
+int sum = 0;
+while (number > 0)
+{
+    int digit = number % 10;
+    sum = sum + digit;
+    number = number / 10;
+}
+Console.WriteLine($"{sum}");
