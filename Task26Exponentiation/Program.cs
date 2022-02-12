a2c6230ceb207ad1f54f0a936c3aec3c72ae571d
aleksey <aleksey7008795@yandex.ru>
2022-02-12 15:24:40 +0300

    Выполнена задача 26 через цикл for

---
+++ Task26Exponentiation/Program.cs
@@ -0,0 +1,18 @@
+﻿// SВозведите число А в натуральную степень B используя цикл
+
+int GetNumber()
+{
+    int num = new Random().Next(1, 5);
+    return num;
+}
+
+int number = GetNumber();
+int degree = GetNumber();
+int result = number;
+
+for (int i = 1; i < degree; i++)
+{
+    result = result * number;
+}
+
+Console.WriteLine($"Число {number} в {degree} степени = {result}");
