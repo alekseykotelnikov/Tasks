6e87c4d907ed150e22feeb1368ea9176b0f89e9a
aleksey <aleksey7008795@yandex.ru>
2022-02-13 15:19:57 +0300

    выполнена задача 30 через 3 метода

---
+++ Task30CubeNumber%2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(-11, 11);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    for (int j = 0; j < col.Length; j++)
+    {
+        Console.Write($"{col[j]} ");
+    }
+    Console.WriteLine();
+}
+
+void PrintEvenNumbers(int[] collect)
+{
+    for (int count = 0; count < collect.Length; count++)
+    {
+        int n = collect[count];// Вводим переменную для красивого тображения результата
+        collect[count] = collect[count] * collect[count] * collect[count];
+        if (collect[count] % 2 == 0)
+        {
+            Console.WriteLine($"Число {n} в кубе = {collect[count]}");
+        }
+        else
+        {
+            Console.Write("");
+        }
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
+Console.WriteLine("Задан следующий массив чисел:");
+PrintArray(array);
+Console.WriteLine("Кубы чисел, из заданного массива, заканчивающихся на четную цифру, это ");
+PrintEvenNumbers(array);
