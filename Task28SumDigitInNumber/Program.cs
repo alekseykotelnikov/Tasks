b0c20b42eaea26beba05ab7ec3a21aac5aad472b
aleksey <aleksey7008795@yandex.ru>
2022-02-13 16:15:07 +0300

    Добавлен диалог с пользователем, если он введет отрицательное число

--- Task28SumDigitInNumber/Program.cs
+++ Task28SumDigitInNumber/Program.cs
@@ -1,10 +1,15 @@
 ﻿// Подсчитать сумму цифр в числе
-int number = -1;
-while (number < 0)
+int number = 0;
+while (number == 0)
 {
     Console.Write("Введите положительное число, чтобы не спорить о знаке \" - \"  ");
-    string? numberAStr = Console.ReadLine();
+    string numberAStr = Console.ReadLine();
     int.TryParse(numberAStr, out number);
+    if (number < 0)
+    {
+         Console.WriteLine("Просили же ввести положительное число, знак минус не будет учтен ");
+        number = number * -1;
+    }
 }
 
 Console.Write($"Сумма цифр в числе {number} = ");
