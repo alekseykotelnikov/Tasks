e1dfd5fb519e63dc1d4a9858751b332966e3d52d
aleksey <aleksey7008795@yandex.ru>
2022-02-13 15:46:25 +0300

    выполнена задача 32 через foreach

---
+++ Task32MassiveElements01/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+
+int[] InitArray()
+{
+    int[] array = new int[8];
+    Random numberRan = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = numberRan.Next(0, 2);
+    }
+    return array;
+}
+
+void PrintThroughtForeachLoop(int[] array)
+{
+    foreach (int item in array)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+int[] array = InitArray();
+Console.WriteLine("Массив из 8 элементов, заполненных нулями и единицами:");
+PrintThroughtForeachLoop(array);
