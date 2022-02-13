d618ae32d50d1c0a276d3d8b4874228c711e9894
aleksey <aleksey7008795@yandex.ru>
2022-02-13 16:51:24 +0300

    добавлен ответ на введение текста

--- Task28SumDigitInNumber/Program.cs
+++ Task28SumDigitInNumber/Program.cs
@@ -1,23 +1,25 @@
 ﻿// Подсчитать сумму цифр в числе
-int number = 0;
-while (number == 0)
+
+Console.Write("Введите положительное число, чтобы не спорить о знаке \" - \":  ");
+string numberAStr = Console.ReadLine();
+if (int.TryParse(numberAStr, out int number))
 {
-    Console.Write("Введите положительное число, чтобы не спорить о знаке \" - \"  ");
-    string numberAStr = Console.ReadLine();
-    int.TryParse(numberAStr, out number);
     if (number < 0)
     {
-         Console.WriteLine("Просили же ввести положительное число, знак минус не будет учтен ");
+        Console.WriteLine("Просили же ввести положительное число, знак минус не будет учтен ");
         number = number * -1;
     }
+    Console.Write($"Сумма цифр в числе {number} = ");
+    int sum = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        sum = sum + digit;
+        number = number / 10;
+    }
+    Console.WriteLine($"{sum}");
 }
-
-Console.Write($"Сумма цифр в числе {number} = ");
-int sum = 0;
-while (number > 0)
+else
 {
-    int digit = number % 10;
-    sum = sum + digit;
-    number = number / 10;
+    Console.WriteLine("Введено не число, программа будет остановлена");
 }
-Console.WriteLine($"{sum}");
