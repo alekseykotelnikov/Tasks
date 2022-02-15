93b4f36434ff5dd0148ea0df8e29e8e60329f34d
aleksey <aleksey7008795@yandex.ru>
2022-02-15 20:01:36 +0300

    Выполнена задача через 3 метода и добавлением console clear

---
+++ Task34InverseNumbersinMassive/Program.cs
@@ -0,0 +1,37 @@
+﻿// Написать программу замену элементов массива на противоположные
+Console.Clear();
+int[] InitArr()
+{
+    var arr = new int[10];
+    var randomizer = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = randomizer.Next(1, 100);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+int[] ChangeElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] - arr[i] * 2;
+    }
+    return arr;
+}
+
+int[] initarr = InitArr();
+Console.WriteLine("Задан массив чисел: ");
+PrintArr(initarr);
+int[] newarr = ChangeElements(initarr);
+Console.WriteLine();
+Console.WriteLine("Массив с противоположными элементами: ");
+PrintArr(newarr);
