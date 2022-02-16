4c8668191b5022535e959e927ff12bc0af67b51e
aleksey <aleksey7008795@yandex.ru>
2022-02-16 22:18:15 +0300

    Добавил вариант 34 задачи с сменами позиций первый/последний

---
+++ Task34InverseVersion2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу замену элементов массива на противоположные
+//Вариант, где противоположными являются первое/последнее число и т.д.
+int[] InitArr()
+{
+    var arr = new int[10];
+    var randomizer = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = randomizer.Next(1, 100);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+void InverseMassive(int[] arr)
+{
+    for (int i = arr.Length - 1; i >= 0; i--)
+    Console.Write($"{arr[i]} ");
+}
+
+int[] numbersMassive = InitArr();
+Console.WriteLine("Задан массив: ");
+PrintArr(numbersMassive);
+Console.WriteLine("Инверсия массива будет следующей: ");
+InverseMassive(numbersMassive);
