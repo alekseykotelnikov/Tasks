25bf823aba0e25bff28d0ba9a510f6f32d215460
aleksey <aleksey7008795@yandex.ru>
2022-02-19 18:32:00 +0300

    Выполнено задание через цикл с защитой от банана

---
+++ Task42CountNumberKeyboard/Program.cs
@@ -0,0 +1,25 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+Console.Clear();
+System.Console.WriteLine();
+
+int numberTry = 5;
+int count = 0;
+while (numberTry > 0)
+{
+    Console.Write("Введите число ");
+    string valueStr = Console.ReadLine();
+    if (int.TryParse(valueStr, out int valueInt))
+    {
+        if (valueInt > 0)
+        {
+            count++;
+        }
+        numberTry--;
+    }
+    else
+    {
+        Console.Write("");
+
+    }
+}
+Console.WriteLine($"Количество чисел больше 0, введенных с клавиатуры = {count}");
