80448d5e20035dbe9b776f8bfe310e8732188d59
aleksey <aleksey7008795@yandex.ru>
2022-02-19 19:41:02 +0300

    Выполнена задача с точкой пересечения и защитой от 0

---
+++ Task44CrossCoord/Program.cs
@@ -0,0 +1,25 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+Console.Clear();
+System.Console.WriteLine();
+double GetNumber()
+{
+    double number = new Random().Next(1, 10);
+    return number;
+}
+
+double k1 = GetNumber();
+double b1 = GetNumber();
+double k2 = GetNumber();
+double b2 = GetNumber();
+if (k1 - k2 != 0)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = (k1 * x) + b1;
+    Console.WriteLine($"Заданы переменные:");
+    Console.WriteLine($" k1 = {k1};\n k2 = {k2};\n b1 = {b1};\n b2 = {b2}");
+    Console.WriteLine($"Точка пересечения двух прямых, это x = {x}, y = {y}");
+}
+else
+{
+    Console.WriteLine($"Запустите программу заново!");
+}
