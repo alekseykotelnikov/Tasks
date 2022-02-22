0fafbd0e5c99f5be842bb4d4789dbf498909e151
AlekseyKotelnikov <aleksey7008795@yandex.ru>
2022-02-22 16:47:17 +0300

    Решена задача двумерный массив через методы

---
+++ Task48Massive2EvenNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+Random rand = new Random();
+int[,] FillArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(1, 100);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = rand.Next(2, 5);
+int n = rand.Next(2, 5);
+int[,] evenArray = FillArray(m, n);
+PrintArray(evenArray);
