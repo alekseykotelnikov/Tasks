805357a8ba6ace5e3caca726c9eea2a59945a556
aleksey <aleksey7008795@yandex.ru>
2022-02-23 16:09:47 +0300

    Выполнена задача по поиску суммы элементов главной диагонали  двумерного массива

---
+++ Task54MainDiagDoubleMas/Program.cs
@@ -0,0 +1,47 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали
+Console.Clear();
+Console.WriteLine();
+Random rand = new Random();
+int[,] FillArray()
+{
+    int[,] array = new int[5, 5];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(1, 10);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void SumMatrixDiag(int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                result = result + array[i, j];
+                Console.WriteLine($"Число {array[i, j]} будет входить в сумму главной диагонали");
+            }
+        }
+    }
+    Console.WriteLine($"Сумма всех элементов, входящих в главную диагональ = {result}");
+}
+int[,] doubleMass = FillArray();
+Console.WriteLine("Задан двумерный массив следующих элементов:");
+PrintArray(doubleMass);
+SumMatrixDiag(doubleMass);
