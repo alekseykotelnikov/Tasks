1f75494d7a62bd8bfaac16ceb4a649ffa5dd422d
aleksey <aleksey7008795@yandex.ru>
2022-02-26 16:40:50 +0300

    Выполнена задача с поиском строки с наименьшей суммой элементов двойного массива

---
+++ Task59MinRowInDoubleMass/Program.cs
@@ -0,0 +1,59 @@
+﻿// В прямоугольной матрице найти строку с наименьшей суммой элементов.
+Console.Clear();
+Console.WriteLine();
+Random rand = new Random();
+int[,] FillArray()
+{
+    int[,] array = new int[3, 3];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(1, 10);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void SortArray(int[,] array)
+{
+    int result = 0;
+    int raw = 1;
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i, j];
+        }
+        Console.WriteLine($"Сумма строки {i + 1} = {sum}");
+        if (i == 0)
+        {
+            result = sum;
+        }
+        else
+        {
+            if (sum < result)
+            {
+                result = sum;
+                raw = i + 1;
+            }
+        }
+    }
+    Console.WriteLine($"Строка с наименьшей суммой элементов: {raw}");
+}
+int[,] doubleMass = FillArray();
+Console.WriteLine("Задан двумерный массив следующих элементов:");
+PrintArray(doubleMass);
+SortArray(doubleMass);
