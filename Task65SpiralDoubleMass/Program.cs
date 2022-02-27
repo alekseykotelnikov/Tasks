7eb079b9f62ad6c868351dc8f180707aa9848b57
aleksey <aleksey7008795@yandex.ru>
2022-02-27 12:38:34 +0300

    Выполнена задача спирального массива через большое количество while

---
+++ Task65SpiralDoubleMass/Program.cs
@@ -0,0 +1,78 @@
+﻿// Спирально заполнить двумерный массив:
+Console.Clear();
+Console.WriteLine();
+int[,] FillArray()
+{
+    int[,] array = new int[4, 4];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = 0;
+        }
+    }
+    return array;
+}
+
+void SpirArray(int[,] array)
+{
+    int i = 0;
+    int j = 0;
+    int firstnumber = 1;
+    while (j < array.GetLength(1) - 1)
+    {
+        array[i, j] = firstnumber;
+        firstnumber++;
+        j++;
+    }
+    while (i < array.GetLength(0) - 1)
+    {
+        array[i, j] = firstnumber;
+        firstnumber++;
+        i++;
+    }
+    while (j > 0)
+    {
+        array[i, j] = firstnumber;
+        firstnumber++;
+        j--;
+    }
+    while (i > 0)
+    {
+        array[i, j] = firstnumber;
+        firstnumber++;
+        i--;
+    }
+    i++;
+    j++;
+    while (j < array.GetLength(1) - 1)
+    {
+        array[i, j] = firstnumber;
+        firstnumber++;
+        j++;
+    }  
+    while (j > 1)
+    {
+        array[i + 1, j -1] = firstnumber;
+        firstnumber++;
+        j--;
+    }
+}
+void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($" {array[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+int[,] firstMatrix = FillArray();
+Console.WriteLine("Задан двумерный массив следующих элементов:");
+PrintArray(firstMatrix);
+Console.WriteLine();
+SpirArray(firstMatrix);
+Console.WriteLine("Расположение элементов по спирали:");
+PrintArray(firstMatrix);
