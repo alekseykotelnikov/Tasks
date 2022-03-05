7a73b07f7b40cbf1d804e7b803fad56b0fd09c68
aleksey <aleksey7008795@yandex.ru>
2022-03-05 15:56:58 +0300

    Выполнена сортировка массива на четные числа

--- Sort2Array/Program.cs
+++ Sort2Array/Program.cs
@@ -34,6 +34,34 @@ string SortArray(int[] array)
     return arrayB;
 }
 
+int CountEvenNumbers(int[] array)   // Подсчет количества четных элементов исходного массива
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] CreateEvenNumbersArray(int count, int[] firstArray)  // Создание нового массива из четных элементов исходного массива
+{
+    int[] arrayEven = new int[count];
+    int j = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i] % 2 == 0)
+        {
+            arrayEven[j] = firstArray[i];
+            j++;
+        }
+    }
+    return arrayEven;
+}
+
 int[] array = InitArray();
 Console.WriteLine("Через цикл foreach: ");
 PrintThroughtForeachLoop(array);
